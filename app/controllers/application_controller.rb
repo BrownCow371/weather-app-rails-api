e84e5733ceffd25d1f08719a8fc42f7206a149c7
BrownCow371 <jmbrowncow@gmail.com>
2018-10-26 15:59:44 -0700

    add protect from forgery to App Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+    # Prevent CSRF attacks by raising an exception.
+    # Per FullStackReact - For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
 end
