97267949b3d283e0bb887fffd575687f49658685
BrownCow371 <jmbrowncow@gmail.com>
2018-10-27 19:00:22 -0700

    update routes to match updated actions - current_weather and forecast_weather

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'searches#openweathermap'
+  root 'searches#current_weather'
   # get '/search', to: 'searches#search'
   # post '/search', to: 'searches#openweathermap'
 
-  # scope '/api' do
-  #   get :weather, to: 'searches#openweathermap'
-  # end
+  scope '/api' do
+    get :weather, to: 'searches#current_weather'
+    # get :forecast, to 'searches#forecast_weather'
+  end
 end
