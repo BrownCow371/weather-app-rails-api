5650f465f41fd709dc8ff0654b425e0f504c9a08
BrownCow371 <jmbrowncow@gmail.com>
2018-10-29 12:41:20 -0700

    update searches controller to store needed weather data and to only call third party weather api if it has been at least 15 minute since called for the same zip

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -13,14 +13,49 @@ class SearchesController < ApplicationController
     end
 
     def current_weather
-        @resp =Faraday.get 'https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22' do |req|
-            # req.params['APPID'] = ENV['APPID']
-            # req.params['zip'] = params[:zipcode]
-            # req.params['units'] = 'imperial'
-        end
+        # find and use existing weather if it is more than 15 minutes old. else fetch new data
+    #    
+        existing_weather = Weather.find_by(zip: params[:zip]) 
+        # binding.pry
+        if existing_weather && ((existing_weather.updated_at + (60 * 15)) > Time.now)
+            render :json => existing_weather
+        else
+            # @resp =Faraday.get 'https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22' do |req|
+            @resp = Faraday.get 'http://api.openweathermap.org/data/2.5/weather' do |req|
+                req.params['APPID'] = ENV['APPID']
+                req.params['zip'] = params[:zip]
+                req.params['units'] = 'imperial'
+            end
+        
+            body = JSON.parse(@resp.body)
 
-        body_hash = JSON.parse(@resp.body)
-        render :json => body_hash
+            new_weather_hash = {
+                zip: params[:zip], 
+                date: body["dt"], 
+                description: body["weather"][0]["description"],
+                icon: body["weather"][0]["icon"],
+                main: body["weather"][0]["main"], 
+                temp: body["main"]["temp"],
+                humidity: body["main"]["humidity"],
+                wind_speed: body["wind"]["speed"]
+            }
+            
+            current_weather = Weather.find_or_create_by(zip: params[:zip]) 
+            
+            current_weather.update(new_weather_hash) 
+            
+            # do |w|
+            #     w.date = body["dt"]
+            #     w.description =  body["weather"][0]["description"]
+            #     w.icon = body["weather"][0]["icon"]
+            #     w.main = body["weather"][0]["main"]
+            #     w.temp = body["main"]["temp"]
+            #     w.humidity = body["main"]["humidity"]
+            #     w.wind_speed = body["wind"]["speed"]
+            # end
+            # render :json => body
+            render :json => current_weather
+        end
     end
 end
 
