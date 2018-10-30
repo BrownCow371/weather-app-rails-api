4f6600b59729bc49f501e39b1a1a79abe297bb57
BrownCow371 <jmbrowncow@gmail.com>
2018-10-30 10:32:48 -0700

    try getting right JSON response without serializer

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,5 +2,6 @@ class ActivitiesController < ApplicationController
     def index
         @activities = Activity.all
         render :json => @activities
+        # render json: @activities.to_json(include: :conditions)
     end
 end
