e65f308f7bd45460a9eafb19958f217380463305
BrownCow371 <jmbrowncow@gmail.com>
2018-10-30 09:04:10 -0700

    add code to weather table and change description to desc

--- db/migrate/20181029171445_create_weather_table.rb
+++ db/migrate/20181029171445_create_weather_table.rb
@@ -4,8 +4,9 @@ class CreateWeatherTable < ActiveRecord::Migration[5.2]
       t.string :zip
       t.string :date
       t.string :main 
-      t.string :description
+      t.string :desc
       t.string :icon
+      t.integer :code
       t.float :temp
       t.float :humidity
       t.float :wind_speed
