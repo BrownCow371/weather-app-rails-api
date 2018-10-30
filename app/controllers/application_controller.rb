4c01bdbeb3bad6b18b9944c18f3a0a49e4f51fe6
BrownCow371 <jmbrowncow@gmail.com>
2018-10-30 09:02:13 -0700

    add include Knock::Authenticable to Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::API
     # Prevent CSRF attacks by raising an exception.
     # Per FullStackReact - For APIs, you may want to use :null_session instead.
     # protect_from_forgery with: :exception
+    include Knock::Authenticable
+
 end
