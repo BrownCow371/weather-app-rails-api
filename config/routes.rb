c493580ad86366098b2de1ee54c0409fc7d9eb6a
BrownCow371 <jmbrowncow@gmail.com>
2018-10-30 09:00:42 -0700

    run rails g knock:install and rails g knock:token_controller user which adds user toke to routes and creates config/initializers/knock file

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'searches#current_weather'
-  # get '/search', to: 'searches#search'
-  # post '/search', to: 'searches#openweathermap'
-
+ 
   scope '/api' do
+    post 'user_token' => 'user_token#create'
     get '/weather', to: 'searches#current_weather'
     get '/forecast', to: 'searches#forecast_weather'
     get '/weather/:zip', to: 'searches#current_weather'
     get '/forecast/:zip', to: 'searches#forecast_weather'
+    resources :activities
   end
 end
