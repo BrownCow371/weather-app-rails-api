1c153efd844d264a5173979da890600d8911a86e
BrownCow371 <jmbrowncow@gmail.com>
2018-10-31 07:24:49 -0700

    thin out current weather controller by making private build weather hash method. remove commented notes. comment out forecast action - future functionality.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,27 +1,30 @@
 class SearchesController < ApplicationController
+# for future user authentication
     # before_action :authenticate_user
 
-  
-    def forecast_weather
-        @resp =Faraday.get 'https://samples.openweathermap.org/data/2.5/forecast?zip=94040&appid=b6907d289e10d714a6e88b30761fae22' do |req|
-            # req.params['APPID'] = ENV['APPID']
-            # req.params['zip'] = params[:zipcode]
-            # req.params['units'] = 'imperial'
-        end
+# save working on this functionality for another day:
+    # def forecast_weather
+    #     @resp =Faraday.get 'http://api.openweathermap.org/data/2.5/forecast' do |req|
+    #         req.params['APPID'] = ENV['APPID']
+    #         req.params['zip'] = params[:zip]
+    #         req.params['units'] = 'imperial'
+    #     end
 
-        body_hash = JSON.parse(@resp.body)
-        render :json => body_hash
-    end
+    #     body_hash = JSON.parse(@resp.body)
+    #     render :json => body_hash
+    # end
 
     def current_weather
         # find and use existing weather if it is less than 15 minutes old. else fetch new data
-    #    
+   
         existing_weather = Weather.find_by(zip: params[:zip]) 
-        # binding.pry
+      
         if existing_weather && ((existing_weather.updated_at + (60 * 15)) > Time.now)
+
             render :json => existing_weather
-        # temporarily fetching sample data
+        
         else
+            # temporarily fetching sample data
             @resp =Faraday.get 'https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22' do |req|
             # @resp = Faraday.get 'http://api.openweathermap.org/data/2.5/weather' do |req|
             #     req.params['APPID'] = ENV['APPID']
@@ -29,9 +32,20 @@ class SearchesController < ApplicationController
             #     req.params['units'] = 'imperial'
             end
         
-            body = JSON.parse(@resp.body)
+            body = JSON.parse(@resp.body)         
+            current_weather = Weather.find_or_create_by(zip: params[:zip]) 
+            weather_hash = build_weather_hash(body)
+            current_weather.update(weather_hash) 
+            
+            render :json => current_weather
+        end
+    end
 
-            new_weather_hash = {
+    private
+
+    def build_weather_hash(body)
+
+        {
                 zip: params[:zip], 
                 date: body["dt"], 
                 desc: body["weather"][0]["description"],
@@ -42,33 +56,5 @@ class SearchesController < ApplicationController
                 humidity: body["main"]["humidity"],
                 wind_speed: body["wind"]["speed"]                
             }
-            
-            current_weather = Weather.find_or_create_by(zip: params[:zip]) 
-            
-            current_weather.update(new_weather_hash) 
-            
-            # do |w|
-            #     w.date = body["dt"]
-            #     w.desc =  body["weather"][0]["description"]
-            #     w.icon = body["weather"][0]["icon"]
-            #     w.main = body["weather"][0]["main"]
-            #     w.temp = body["main"]["temp"]
-            #     w.humidity = body["main"]["humidity"]
-            #     w.wind_speed = body["wind"]["speed"]
-            # end
-            # render :json => body
-            render :json => current_weather
-        end
     end
 end
-
-#Forecast Data 
-    # real api address: api.openweathermap.org/data/2.5/forecast?zip={zip code}&units=imperial
-    # http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
-# Current Data:
-    # api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}
-    # recommended that we store data...how do we check to see if we need to fetch new data?
-# req.params['client_id'] = client_id
-# req.params['client_secret'] = client_secret
-# req.params['zip'] = params[:zipcode]
-# req.params['units'] = 'imperial'
