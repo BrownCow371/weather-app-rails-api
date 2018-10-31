43a57a9c738abf2919f7ec5c957ceb604c35c11c
BrownCow371 <jmbrowncow@gmail.com>
2018-10-31 07:25:38 -0700

    comment out routes that are not being used by current app

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   root 'searches#current_weather'
  
   scope '/api' do
-    post 'user_token' => 'user_token#create'
-    get '/weather', to: 'searches#current_weather'
-    get '/forecast', to: 'searches#forecast_weather'
+  # for future user authentication
+    # post 'user_token' => 'user_token#create'
+    # get '/weather', to: 'searches#current_weather'
+    # get '/forecast', to: 'searches#forecast_weather'
     get '/weather/:zip', to: 'searches#current_weather'
-    get '/forecast/:zip', to: 'searches#forecast_weather'
+  # future forecase functionality
+    # get '/forecast/:zip', to: 'searches#forecast_weather'
     resources :activities
   end
 end
