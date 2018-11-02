18826440485cb8e4a83ce7561eadb6fb8b5d9a6b
BrownCow371 <jmbrowncow@gmail.com>
2018-11-02 14:33:46 -0700

    update Activity reference in method to self

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -9,7 +9,7 @@ class Activity < ApplicationRecord
 
         # need to filter activitied based on condition, wind speed, min and max temp
 
-        possible_activities = Activity.all.select do |activity|
+        possible_activities = self.all.select do |activity|
             (activity.min_temp ? weather.temp >= activity.min_temp : true) &&
             (activity.max_temp ? weather.temp <= activity.max_temp : true) &&
             (activity.max_wind_speed ? weather.wind_speed <= activity.max_wind_speed : true) &&
