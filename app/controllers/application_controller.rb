8eeb9eceaf776e131271823733346460102dfe89
BrownCow371 <jmbrowncow@gmail.com>
2018-11-03 10:44:42 -0700

    switch back to ActionController::API. will work on authentication needs in separate branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
+# class ApplicationController < ActionController::Base
 class ApplicationController < ActionController::API
     # Prevent CSRF attacks by raising an exception.
     # Per FullStackReact - For APIs, you may want to use :null_session instead.
     # protect_from_forgery with: :exception
-    include Knock::Authenticable
+    # protect_from_forgery with: :null_session
+    
+# save for future user authentication
+    # include Knock::Authenticable
 
 end
