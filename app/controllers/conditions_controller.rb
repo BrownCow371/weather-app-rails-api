f456fc3be698aa4e7e44622e92f83d22a9742799
BrownCow371 <jmbrowncow@gmail.com>
2018-11-04 20:17:07 -0800

    add logic to render errors if data is missing

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,7 +1,17 @@
 class ConditionsController < ApplicationController
     def index
         @conditions = Condition.all
-        render :json => @conditions
+        if @conditions
+            render(
+                status: 200, 
+                json: @conditions
+            ) 
+        else
+            render( 
+                status: 400, 
+                json: {error: 'No weather conditions found.'}
+            )
+        end
     end
 
 end
