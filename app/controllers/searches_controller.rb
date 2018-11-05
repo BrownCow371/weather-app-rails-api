b26e750abfaaba19aef04fef4ee38211fffadc59
BrownCow371 <jmbrowncow@gmail.com>
2018-11-04 16:29:17 -0800

    add render logic to search_controller actions. to make actions skinnt, build private functions for handling validation of zip code, setting weather object, setting activity object, calling the api and processing the api response. update readme to refelct now location of api call.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,7 @@
 class SearchesController < ApplicationController
 # for future user authentication
     # before_action :authenticate_user
+    before_action :set_weather, only: [:current_weather]
 
 # save working on this functionality for another day:
     # def forecast_weather
@@ -16,41 +17,91 @@ class SearchesController < ApplicationController
 
     def current_weather
         # find and use existing weather if it is less than 15 minutes old. else fetch new data
-   
-        existing_weather = Weather.find_by(zip: params[:zip]) 
-      
-        if existing_weather && ((existing_weather.updated_at + (60 * 15)) > Time.now)
-
-            render :json => existing_weather
-        
-        else
-        # to fetch sample data uncomment these lines and comment out the ones for fetching live data below.
-            # @resp =Faraday.get 'https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22' do |req|
-            # end
-        
-        # to fetch live API data using your APP ID key  - 
-        # be sure to put your APPID=YOURKEY in a .env file at the root of this repository.)
-            @resp = Faraday.get 'http://api.openweathermap.org/data/2.5/weather' do |req|
-                req.params['APPID'] = ENV['APPID']
-                req.params['zip'] = params[:zip]
-                req.params['units'] = 'imperial'
-            end
-        
-            body = JSON.parse(@resp.body)         
-            current_weather = Weather.find_or_create_by(zip: params[:zip]) 
-            weather_hash = build_weather_hash(body)
-            current_weather.update(weather_hash) 
+     
+        if validate_zip 
             
-            render :json => current_weather
+            if @weather && ((@weather.updated_at + (60 * 15)) > Time.now)
+
+                render(
+                    status: 200,
+                    json: @weather
+                )
+            else        
+                @resp = call_weather_api(params[:zip])
+                process_response(@resp)
+            end       
+            
+        else 
+            render status: 400, json: {error:'Expected zipcode to be a 5 digit number.'}
         end
     end
 
     def activity_suggestion
-        activity  = Activity.suggestion(params[:zip])
-        render :json => activity
+
+        if set_activity
+            render(
+                status: 200, 
+                json: @activity
+            ) 
+        else
+            render( 
+                status: 400, 
+                json: {error: 'No activity found for provided zipcode.'}
+            )
+        end
     end
 
+
+
     private
+
+    def set_weather
+        if validate_zip
+            @weather =  Weather.find_by(zip: params[:zip])
+        end
+    end
+
+    def validate_zip
+        params[:zip].match(/\b\d{5}\b/)
+    end
+
+    def set_activity
+        if validate_zip
+            @activity = Activity.suggestion(params[:zip])
+        end
+    end
+
+    def call_weather_api(zip)
+
+        # to fetch sample data uncomment these lines and comment out the ones for fetching live data below.
+            # Faraday.get 'https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22' do |req|
+            # end
+        
+        # to fetch live API data using your APP ID key  - 
+        # be sure to put your APPID=YOURKEY in a .env file at the root of this repository.)
+        Faraday.get 'http://api.openweathermap.org/data/2.5/weather' do |req|
+            req.params['APPID'] = ENV['APPID']
+            req.params['zip'] = zip
+            req.params['units'] = 'imperial'
+        end
+
+    end
+
+    def process_response(response)
+        if  response.status >= 400
+            render status: response.status, json: {error: 'No data found for provided zipcode.'}
+        else 
+            body = JSON.parse(response.body)         
+            @weather = Weather.find_or_create_by(zip: params[:zip]) 
+            weather_hash = build_weather_hash(body)
+            @weather.update(weather_hash) 
+            
+            render(
+                status: 200,
+                json: @weather
+            )
+        end
+    end
 
     def build_weather_hash(body)
 
