85ac0736abfed40aba0ba1b014a59125149f84d1
BrownCow371 <jmbrowncow@gmail.com>
2018-11-12 16:12:17 -0800

    remove reference to knock. add methods for logged_in?, authenticate, current_user and add provate methods for collecting, decoding and acknowledging jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,39 @@
-# class ApplicationController < ActionController::Base
 class ApplicationController < ActionController::API
     # Prevent CSRF attacks by raising an exception.
     # Per FullStackReact - For APIs, you may want to use :null_session instead.
     # protect_from_forgery with: :exception
     # protect_from_forgery with: :null_session
-    
-#  user authentication
-    include Knock::Authenticable
+    def logged_in?
+        !!current_user
+    end
 
+    def authenticate
+        render( 
+                status: 400, 
+                json: {error: 'You are unauthroized to perform this action. Please login.'}
+            ) unless logged_in?
+    end
+
+    def current_user
+        if auth_present?
+            user = User.find(auth["user"])
+            if user
+                @current_user ||= user
+            end
+        end
+
+    end
+
+    private
+        def token
+            request.env["HTTP_AUTHORIZATION"].scan(/Bearer (.*)$/).flatten.last
+        end
+
+        def auth
+            Auth.decode(token)
+        end
+
+        def auth_present?
+            !!request.env.fetch("HTTP_AUTHORIZATION", "").scan(/Bearer/).flatten.first
+        end
 end
