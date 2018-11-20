4b9b36f6f5393729a14c0bca993ac665d7e4a766
BrownCow371 <jmbrowncow@gmail.com>
2018-11-20 12:44:29 -0800

    add comments. update auth_present function to determine if Bearer is empty or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,14 @@ class ApplicationController < ActionController::API
     # Per FullStackReact - For APIs, you may want to use :null_session instead.
     # protect_from_forgery with: :exception
     # protect_from_forgery with: :null_session
+
     def logged_in?
-        !!@current_user
+        # call current_user method
+        !!current_user
     end
 
     def authenticate
+        # send error message if not logged_in
         render( 
                 status: 400, 
                 json: {error: 'You are unauthroized to perform this action. Please login.'}
@@ -15,7 +18,9 @@ class ApplicationController < ActionController::API
     end
 
     def current_user
+        # check if authorization header is present, if so decode token to get user id and set user
         if auth_present?
+            # binding.pry
             user = User.find(auth["user"])
             if user
                 @current_user ||= user
@@ -34,6 +39,8 @@ class ApplicationController < ActionController::API
         end
 
         def auth_present?
-            !!request.env.fetch("HTTP_AUTHORIZATION", "").scan(/Bearer/).flatten.first
+            if request.env.fetch("HTTP_AUTHORIZATION", "").scan(/Bearer/).flatten.first
+                (token == "undefined" || token == "null") ? false : true
+            end
         end
 end
